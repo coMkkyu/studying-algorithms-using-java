1925366802f2dc7bf9debbb2f5fd0029a9ad884a
coMkkyu <bakha1105@gmail.com>
2021-01-21 14:39:59 +0900

    solve problem 2231

---
+++ brute_force/2231/Main.java
@@ -0,0 +1,32 @@
+package _2231;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int size = String.valueOf(N).length();
+		int result = 0;
+		
+		for (int i = N - (9 * size); i < N; i++) {
+			int number = i;
+			int sum = 0;
+			
+			while(number != 0) {
+				sum += number % 10;
+				number /= 10;
+			}
+			
+			if (sum + i == N) {
+				result = i;
+				break;
+			}
+		}
+		
+		System.out.println(result);
+	}
+}
