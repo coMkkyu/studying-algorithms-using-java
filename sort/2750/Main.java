767e6a9fb5daa28e811dca784d0970de40c408ee
coMkkyu <bakha1105@gmail.com>
2021-01-22 16:42:48 +0900

    solve problem 2750

---
+++ sort/2750/Main.java
@@ -0,0 +1,32 @@
+package _2750;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[N];
+		
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = i + 1; j < arr.length; j++) {
+				if(arr[i] > arr[j]) {
+					int temp = arr[j];
+					arr[j] = arr[i];
+					arr[i] = temp;
+				}
+			}
+		}
+		
+		for (int num : arr) {
+			System.out.println(num);
+		}
+	}
+}
