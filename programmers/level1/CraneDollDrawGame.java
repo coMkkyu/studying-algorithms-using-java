fabbf42ad67d047ca498086a1ad2250e6b60723a
coMkkyu <bakha1105@gmail.com>
2021-02-13 15:16:20 +0900

    solve problem 크레인 인형 뽑기 게임

--- programmers/level1/CraneDollDrawGame.java
+++ programmers/level1/CraneDollDrawGame.java
@@ -10,7 +10,7 @@ package Crane_doll_draw_game;
 
 import java.util.Stack;
 
-public class Crane {
+public class CraneDollDrawGame {
 	
 	public static void main(String[] args) {
 		int[][] board = {{0,0,0,0,0},{0,0,1,0,3},{0,2,5,0,1},{4,2,4,4,2,},{3,5,1,3,1}};
